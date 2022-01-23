f936ab0924abf1a08e977200bd8e16d7d1260c86
Alex Groat <alex.groat.95@gmail.com>
2022-01-23 13:21:36 +1030

    working with component styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,11 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
+  //styles: [`
+// h3 {
+  // color: red;
+//}
+//`] can use the styles section to write css here in the component, cant combine style urls and styles, must choose one
 })
 export class AppComponent {
   
